b0573da7dde1eb578c34ab2cf9dbf7b0dbcdc343
Arshvir Mundi <arshvir.mundi@gmail.com>
2024-08-06 21:13:25 -0400

    ....

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  rules: {
-    "max-len": ["error", { code: 120 }],
-    "no-html-link-for-pages": "off",
-    // other rules...
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        yellow: "#FFE500",
+        gray: "#6F6E6E",
+      },
     },
   },
   plugins: [],
