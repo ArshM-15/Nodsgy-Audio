7851cff46c47c6fbc1a776d30aacb63574a3dc28
Arshvir Mundi <arshvir.mundi@gmail.com>
2024-11-04 22:48:51 -0500

    asasd

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -2,13 +2,26 @@ import Stripe from "stripe";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
-import serviceAccount from "../../firebase/serviceAccountKey.json"; // Adjust path as necessary
+// import serviceAccount from "../../firebase/serviceAccountKey.json"; // Adjust path as necessary
 
 // Initialize Stripe
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2024-09-30.acacia",
 });
 const endpointSecret = process.env.WEBHOOK_SECRET;
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+};
 
 // Initialize Firebase Admin SDK only if it hasn't been initialized yet
 if (!getApps().length) {
