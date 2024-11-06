35e112e6114ad72dfdb5a5c73e3565c7325c9fb2
Arshvir Mundi <arshvir.mundi@gmail.com>
2024-11-05 19:09:32 -0500

    ..

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -10,17 +10,17 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 });
 const endpointSecret = process.env.WEBHOOK_SECRET;
 const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-  universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  type: process.env.GOOGLE_FIREBASE_TYPE,
+  project_id: process.env.GOOGLE_FIREBASE_PROJECT_ID,
+  private_key_id: process.env.GOOGLE_FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.GOOGLE_FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.GOOGLE_FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.GOOGLE_FIREBASE_CLIENT_ID,
+  auth_uri: process.env.GOOGLE_FIREBASE_AUTH_URI,
+  token_uri: process.env.GOOGLE_FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.GOOGLE_FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.GOOGLE_FIREBASE_CLIENT_X509_CERT_URL,
+  universe_domain: process.env.GOOGLE_FIREBASE_UNIVERSE_DOMAIN,
 };
 
 // Initialize Firebase Admin SDK only if it hasn't been initialized yet
