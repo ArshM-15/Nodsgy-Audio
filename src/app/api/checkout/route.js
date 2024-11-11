0eee03bc54ecc5b3e8be264f30ec46cc55d46771
Arshvir Mundi <arshvir.mundi@gmail.com>
2024-11-11 15:18:24 -0500

    ..

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -6,9 +6,10 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 export async function POST(req) {
   try {
     const { priceId, numOfTokens, amountSpent, userUid } = await req.json();
-    
-    const host = req.headers.get("host");
-    const protocol = host.includes("localhost") ? "http" : "https"; // Use http for local dev, https for production
+
+    // Replace host with nodsgy.com for production, regardless of the environment
+    const domain = "nodsgy.com";
+    const protocol = process.env.NODE_ENV === "development" ? "http" : "https"; // Use http for local dev, https for production
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -19,8 +20,8 @@ export async function POST(req) {
         },
       ],
       mode: "payment",
-      success_url: `${protocol}://${host}/success/${numOfTokens}`, // Constructed success URL
-      cancel_url: `${protocol}://${host}`, // Constructed cancel URL
+      success_url: `${protocol}://${domain}/success/${numOfTokens}`, // Use nodsgy.com for success URL
+      cancel_url: `${protocol}://${domain}`, // Use nodsgy.com for cancel URL
       metadata: {
         user_uid: userUid,
         num_of_tokens: numOfTokens,
