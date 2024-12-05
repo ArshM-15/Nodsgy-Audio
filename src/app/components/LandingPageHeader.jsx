18bfa3db3a7d81744c80511322fc42da79205ef7
Arshvir Mundi <arshvir.mundi@gmail.com>
2024-12-04 19:34:36 -0500

    asd

--- src/app/components/LandingPageHeader.jsx
+++ src/app/components/LandingPageHeader.jsx
@@ -3,11 +3,11 @@ export default function LandingPageHeader() {
     <div className="md:mt-40 mt-10">
       <h1 className="font-bold text-[35px] text-center w-[85%] mx-auto">
         Tough concepts made
-        <span className="text-yellow"> Simple </span>
+        <span className="text-yellow"> simple </span>
         for you
       </h1>
       <h2 className="text-gray font-semibold sm:text-[25px] text-[22px] mx-auto sm:w-[60%] w-[90%] mt-5 text-center">
-        Upload your PDF, PowerPoint, or slides, and Nodsgy will convert them
+        Upload your PDF, PowerPoint, or Slides, and Nodsgy will convert them
         into the simplest explanations you've ever heard
       </h2>
     </div>
